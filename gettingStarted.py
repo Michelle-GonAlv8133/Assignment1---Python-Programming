b3744ca71284a666d1a78cc68a44e4072f720bb2
Michelle-GonAlv8133 <msg8133@nyu.edu>
2025-09-13 11:04:57 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,25 +6,24 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "NO"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "NO"
+        answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "#lab-phython-getting-started"
+        answer = "#lab-python-getting-started"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "NO"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "NO"
+        answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "2439825fa8b6710622d1178881f45c35a69eadeb1facb0f2d9b67b88894e2e9e"
     #Be honest, I am confused how to do respond this. 
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "7"   
-
+        answer = "5"   
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"   
+        answer = "3"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -49,4 +48,5 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 
